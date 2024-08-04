762f5ec1d24da1b9046b7fc5518398d5b5679e77
이현규 <gusrb2776@naver.com>
2024-08-04 15:11:54 +0900

    initial project

---
+++ src/main/java/study/lock_study/LockStudyApplication.java
@@ -0,0 +1,13 @@
+package study.lock_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/lock_study/LockStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.lock_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
